a8b99d997080af8d759dfb265541f74e90947631
Daenielle May Felipe <daeniellemay.felipe@edu.sait.ca>
2022-10-19 12:43:58 -0600

    Added icon

---
+++ README.md
@@ -0,0 +1,7 @@
+Craving for pizza? This React based landing page makes the user crave for pizza for its eye-catching user interface.
+
+- Author: Daenielle May Felipe
+
+![City Delicatessen](./assets/images/readme-img.png)
+
+**Deployment**
---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const handleClick = () => {
+  alert("Coming soon!");
+};
+
+//Mobile Nav
+const toggleMenu = () => {
+  const toggleMenu = document.querySelector(".nav-bars");
+  const navLinks = document.querySelectorAll(".nav-link");
+  const navigation = document.querySelector(".nav-menu");
+  //Adds active class to the toggle menu and navigation
+  toggleMenu.classList.toggle("active");
+  navigation.classList.toggle("active");
+  // For each links clicked, remove 'active' class from the toggle menu and navigation
+  navLinks.forEach((n) => {
+    n.addEventListener("click", () => {
+      toggleMenu.classList.remove("active");
+      navigation.classList.remove("active");
+    });
+  });
+};
